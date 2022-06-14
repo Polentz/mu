162449ce5bc400566c6bf00bf67a037b8c009477
Giulia Polenta <giuliapolenta@gmail.com>
2022-06-14 15:20:18 +0200

    implement js and links behaviour

--- script.js
+++ script.js
@@ -1,8 +1,9 @@
 const menu = document.querySelector(".btn--open");
 const submenu = document.querySelector(".content--open");
 const buttons = document.querySelectorAll(".nav--link");
+const closeButton = document.querySelector(".btn--close");
 const navLinks = document.querySelectorAll(".js-href");
-const closeButton = document.querySelector(".btn--close")
+// const contentBlocks = document.querySelectorAll(".content--block");
 
 
 if (menu) {
@@ -23,85 +24,60 @@ if (buttons) {
     });
 }
 
+if (closeButton) {
+    closeButton.addEventListener("click", (event) => {
+        event.preventDefault();
+        if (history.length) {
+            history.back();
+            return false;
+        }
+    });
+}
+
+if (navLinks) {
+    navLinks.forEach(link => {
+        link.addEventListener("click", (event) => {
+            event.preventDefault();
+
+            const href = link.getAttribute("href");
+            const element = document.getElementById(href);
+            const elementId = element.getAttribute("id");
+
+            element.scrollIntoView({
+                behavior: "smooth"
+            });
+            if (href === elementId) {
+                buttons.forEach(b => {
+                    b.addEventListener("click", () => {
+                        buttons.forEach(btn => {
+                            btn.classList.remove("active");
+                        })
+                        b.classList.add("active");
+                    });
+                });
+            }
+        });
+    });
+}
+
+
 // const inViewport = (e) => {
 //     const elementRect = e.getBoundingClientRect();
 //     return (elementRect.top < innerHeight && elementRect.bottom > 0);
 // };
 
-navLinks.forEach(link => {
-    link.addEventListener("click", (event) => {
-        event.preventDefault();
 
-        const href = link.getAttribute("href");
-        const element = document.getElementById(href);
-        const elementId = element.getAttribute("id");
-        console.log(href)
-        console.log(element)
-        console.log(elementId)
+// if (contentBlocks) {
+//     contentBlocks.forEach(content => {
+//         if (content.hasAttribute("id")) {
 
-        element.scrollIntoView({
-            behavior: "smooth"
-        });
-        if (href === elementId) {
-            buttons.forEach(b => {
-                b.addEventListener("click", () => {
-                    buttons.forEach(btn => {
-                        btn.classList.remove("active");
-                    })
-                    b.classList.add("active");
-                });
-            });
-        }
-
-    });
-
-});
-
-closeButton.addEventListener("click", (event) => {
-    event.preventDefault();
-    if (history.length) {
-        history.back();
-        return false;
-    }
-})
-
-
-
-
-// window.addEventListener("scroll", () => {
-//     const sections = document.querySelectorAll(".content--block");
-//     const sectionWithId = sections.querySelectorAll('[id]');
-//     console.log(sectionWithId)
-
-// })
-
-
-
-// window.addEventListener("scroll", () => {
-//     const sections = document.querySelectorAll(".content--block");
-//     sections.forEach(section => {
-//         if (section.hasAttribute("id") && section.inViewport) {
-
-//             console.log("bau")
+//             const position = content.offsetTop;
+//             const contentId = content.getAttribute("id");
+//             console.log(content)
+//             console.log(position)
+//             console.log(contentId)
+            
 //         }
 //     });
-// })
 
-
-// const inViewport = (e) => {
-//     const elementRect = e.getBoundingClientRect();
-//     return (elementRect.top < innerHeight && elementRect.bottom > 0);
-// };
-
-// document.addEventListener("scroll", () => {
-//     if (section.inViewport) {
-//         buttons.forEach(b => {
-//             b.addEventListener("click", () => {
-//                 buttons.forEach(btn => {
-//                     btn.classList.remove("active");
-//                 })
-//                 b.classList.add("active");
-//             });
-//         });
-//     }
-// })
+// }
