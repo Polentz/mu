cf4a1223bf3be6d0ee3b07bc1e66f0d0c30eaea7
Giulia Polenta <giuliapolenta@gmail.com>
2022-06-14 12:14:25 +0200

    test history.back condition

--- script.js
+++ script.js
@@ -1,6 +1,8 @@
 const menu = document.querySelector(".btn--open");
 const submenu = document.querySelector(".content--open");
 const buttons = document.querySelectorAll(".nav--link");
+const navLinks = document.querySelectorAll(".js-href");
+const closeButton = document.querySelector(".btn--close")
 
 
 if (menu) {
@@ -11,7 +13,6 @@ if (menu) {
 }
 
 if (buttons) {
-    // buttons[0].classList.add("active");
     buttons.forEach(b => {
         b.addEventListener("click", () => {
             buttons.forEach(btn => {
@@ -22,14 +23,84 @@ if (buttons) {
     });
 }
 
+// const inViewport = (e) => {
+//     const elementRect = e.getBoundingClientRect();
+//     return (elementRect.top < innerHeight && elementRect.bottom > 0);
+// };
+
+navLinks.forEach(link => {
+    link.addEventListener("click", (event) => {
+        event.preventDefault();
+
+        const href = link.getAttribute("href");
+        const element = document.getElementById(href);
+        const elementId = element.getAttribute("id");
+        console.log(href)
+        console.log(element)
+        console.log(elementId)
+
+        element.scrollIntoView({
+            behavior: "smooth"
+        });
+        if (href === elementId) {
+            buttons.forEach(b => {
+                b.addEventListener("click", () => {
+                    buttons.forEach(btn => {
+                        btn.classList.remove("active");
+                    })
+                    b.classList.add("active");
+                });
+            });
+        }
+
+    });
+
+});
+
+closeButton.addEventListener("click", (event) => {
+    if (history.length) {
+        history.back();
+        return false;
+    }
+})
 
 
-// buttons[0].classList.add("active");
-// buttons.forEach(b => {
-//     b.addEventListener("click", () => {
-//         buttons.forEach(btn => {
-//             btn.classList.remove("active");
-//         })
-//         b.classList.add("active");
+
+
+// window.addEventListener("scroll", () => {
+//     const sections = document.querySelectorAll(".content--block");
+//     const sectionWithId = sections.querySelectorAll('[id]');
+//     console.log(sectionWithId)
+
+// })
+
+
+
+// window.addEventListener("scroll", () => {
+//     const sections = document.querySelectorAll(".content--block");
+//     sections.forEach(section => {
+//         if (section.hasAttribute("id") && section.inViewport) {
+
+//             console.log("bau")
+//         }
 //     });
-// });
+// })
+
+
+// const inViewport = (e) => {
+//     const elementRect = e.getBoundingClientRect();
+//     return (elementRect.top < innerHeight && elementRect.bottom > 0);
+// };
+
+// document.addEventListener("scroll", () => {
+//     if (section.inViewport) {
+//         buttons.forEach(b => {
+//             b.addEventListener("click", () => {
+//                 buttons.forEach(btn => {
+//                     btn.classList.remove("active");
+//                 })
+//                 b.classList.add("active");
+//             });
+//         });
+//     }
+// })
